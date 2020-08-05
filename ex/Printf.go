1ee108cce1f07539413ce2e5317f2dd7df061fa1
kk <kk@alpha.pw>
2020-08-05 17:09:11 +0800

    ✨ printf.go

---
+++ ex/Printf.go
@@ -0,0 +1,9 @@
+// 
+package main
+import "fmt"
+// main 
+func main () {
+	fmt.Printf ("My weight on the surface of Mars is %v lbs,", 149.0 * 0.3783)
+	fmt.Printf (" and I would be %v years old.\n", 41 * 365 / 687)
+	fmt.Printf ("My weight on the surface of %v is %v lbs.", "Earth", 149.0)
+}
